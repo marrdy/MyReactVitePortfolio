61bd87481f0714214523c42cd2caa448e41d0cd1
marc eddyson <cruz.marceddyson.eccbscs@gmail.com>
2024-08-24 21:10:35 +0800

    idk tbh

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import meDp from '@assets/meDp.jpg'; 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/MyReactVitePortfolio/"
 })
